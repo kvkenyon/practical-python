04723690546c495987e805b658f79d442a387691
Kevin Kenyon <kwkenyon@us.ibm.com>
2021-03-13 15:25:25 -0600

    Complete mortgage calculator

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,43 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.7/1.8/1.9/1.10/1.11/1/12
+
+extra_payment_start_month = 61
+extra_payment_end_month = extra_payment_start_month + (12 * 4) 
+extra_payment = 1000
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+month = 1 
+
+while principal > 0:
+    extra = 0
+    if extra_payment_start_month <= month < extra_payment_end_month:
+        extra = extra_payment
+
+    principal = principal * (1+rate/12)
+
+    monthly_payment = payment + extra
+
+    if monthly_payment > principal:
+        monthly_payment = principal
+
+    principal = principal - monthly_payment
+
+    total_paid = total_paid + monthly_payment 
+
+    print(month, round(total_paid, 2), round(principal, 2))
+    month = month + 1
+
+print('Total paid', round(total_paid, 2))
+print('Months:', month - 1)
+
+# 1.9
+# Total paid 880074.1
+# Months: 310
+
+# 1.12
+# Since the false string is not empty we get True
