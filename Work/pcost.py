c04788e1ea63edca24ff45527c2d0988d1c38a6b
Kevin Kenyon <kwkenyon@us.ibm.com>
2021-03-13 17:37:47 -0600

    Convert pcost into function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,16 +2,21 @@
 #
 # Exercise 1.27
 
-import os
 
-print(os.getcwd())
-with open('Data/portfolio.csv', 'rt') as f:
-    next(f)
-    total_price = 0.0
-    for line in f:
-        row = line.split(',')
-        if row and len(row) == 3:
-            shares = int(row[1])
-            price = float(row[2])
-            total_price = total_price + (shares * price)
-    print(f'Total cost: {total_price}')        
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        next(f)
+        total_price = 0.0
+        for line in f:
+            row = line.split(',')
+            try:
+                shares = int(row[1])
+                price = float(row[2])
+                total_price = total_price + (shares * price)
+            except ValueError:
+                print('Invalid input:', line)
+        return total_price
+    raise RuntimeError
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
