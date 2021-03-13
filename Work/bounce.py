3dfe6af049d0819effc36dc8f994bdf597527f61
Kevin Kenyon <kwkenyon@us.ibm.com>
2021-03-13 12:58:57 -0600

    Solve first two exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # ball starting height
+bounce_back = 3/5 # bounce back rate
+num_bounces = 10
+bounce = 1 # bounce number
+
+while bounce <= num_bounces:
+    height *= bounce_back
+    print(bounce, round(height, 4)) 
+    bounce += 1
+    
