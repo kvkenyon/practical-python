8e54d7a85086f754c9b221490ca0ae0625d7a0e9
Kevin Kenyon <kwkenyon@us.ibm.com>
2021-03-13 18:36:07 -0600

    Create read portfolio function

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,26 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+        return portfolio
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+portfolio = read_portfolio(filename)
+total = 0.0
+for name, nshares, price in portfolio:
+    total += nshares * price
+print(total)
